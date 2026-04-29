91b91f9bad42f5e8332ac1e871c9084fa183b267
BandanaBandieto <banda.griffiths@gmail.com>
2026-04-29 12:14:43 +0200

    Create Business.ts

---
+++ src/Business.ts
@@ -0,0 +1,14 @@
+export const business = {
+  name: "CrystalClear",
+  tagline: "Streeploos schone ramen, elke keer weer.",
+  phone: "+31000000000",
+  phoneDisplay: "[Telefoonnummer]",
+  email: "info@crystalclear.nl",
+  emailDisplay: "[E-mailadres]",
+  region: "Noord-Holland",
+  cities: [
+    { name: "Heemskerk", desc: "Glasbewassing voor woningen en bedrijven in heel Heemskerk." },
+    { name: "Uitgeest", desc: "Streeploos schone ramen in Uitgeest en omliggende dorpen." },
+    { name: "Beverwijk", desc: "Professionele glazenwasser actief in heel Beverwijk." },
+  ],
+};
